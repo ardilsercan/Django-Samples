b7db939d9fa37207f52b7ae01fe6528c93f8d1f7
ardilsercan <ardilsercan@gmail.com>
2021-03-20 12:57:09 +0300

    Model Serializer and Validation
    added -- Model Serializer and Validation

--- djangotodoapp/todo/api/serializers.py
+++ djangotodoapp/todo/api/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from todo.models import Todo
 
-class TodoSerializer(serializers.Serializer):
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = '__all__'  # GET all data
+        # fields = ['title', 'completed'] # GET only title and completed value
+        #exclude = ['title']
+        read_only_fields = ['id']
+
+
+
+# -------------- STANDARD SERIALIZER -------------- #
+
+class TodoDefaultSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField()
     completed = serializers.BooleanField()
@@ -15,3 +28,19 @@ class TodoSerializer(serializers.Serializer):
         instance.completed= validated_data.get('completed', instance.completed)
         instance.save()
         return instance
+
+# -------------- STANDARD SERIALIZER -------------- #
+
+     # -------------- VALIDATION -------------- # 
+
+    def validate(self, data): #object seviyesinde
+        if data['title'] == data['completed']:
+            raise serializers.ValidationError('Baslik ve durum alanları aynı olamaz. Lütfen farklı bir başlık giriniz.')
+        return data
+
+    def validate_title(self, value): #value seviyesinde
+        if len(value) < 4:
+          raise serializers.ValidationError(f'Başlık alanı 4 karakterden az olamaz. Giriş yapılan karakter sayısı: {len(value)}')
+        return value 
+
+    # -------------- VALIDATION -------------- # 
--- djangotodoapp/todo/api/views.py
+++ djangotodoapp/todo/api/views.py
@@ -22,7 +22,7 @@ class TodoListCreateAPIView(APIView):
         if serializer.is_valid(): 
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED) 
-        return Response(status = status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 
 class TodoDetailAPIView(APIView):
